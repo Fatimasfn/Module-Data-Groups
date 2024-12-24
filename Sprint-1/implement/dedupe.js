25358320a0786a4814c9c35ce073b2fc0b403156
fatima safana <asksiyam@gmail.com>
2024-12-24 05:59:26 +0000

    testing no duplicates

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+module.exports = dedupe;
+
+
+function dedupe(list) {
+//given an empty array, it returns an empty array
+        if (list.length === 0 ) {
+            return list 
+//given an array with no duplicates, it returns a copy of the original array
+        } else if (list.length !== 0){
+            let occurence = 0
+            for (i = 0, i < list.length; indexBeingChecked = 0,  i++)
+                list[i] === list[1]
+                                                                                                                  
+        }
+}
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,11 +16,17 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+test("given an empty array, it returns an empty array", () => {
+    expect(dedupe([])).toEqual([]);
+  });
+
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
+test("given an array with no duplicates, it returns a copy of the original array", () => {
+    expect(dedupe(['a', 'b', 'c'])).toEqual(['a', 'b', 'c']);
+  });
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
