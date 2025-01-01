d96aac45660e762ca705b4ebea5d8f6907e65b7b
fatima safana <asksiyam@gmail.com>
2025-01-01 15:06:07 +0000

    level 1 implementation

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,22 @@ const images = [
 
 
 // Write your code here
+
+//1: get element needed
+const backwardButton = document.querySelector("#backward-btn");
+const forwardButton = document.querySelector("#forward-btn");
+const carouselImage = document.querySelector("#carousel-img");
+
+let currentIndex = 0;
+
+backwardButton.addEventListener("click", () => {
+// access the images array and replace current image with next image on the list.
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  carouselImage.src = images[currentIndex]; // Update image source
+});
+
+forwardButton.addEventListener("click", () => {
+// access the images array and replace current image with next image on the list.
+  currentIndex = (currentIndex + 1 + images.length) % images.length;
+  carouselImage.src = images[currentIndex]; // Update image source
+});
