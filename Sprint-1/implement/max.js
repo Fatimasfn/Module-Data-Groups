b4dceecc41f9c9f69d988eff095b511a2437c906
fatima safana <asksiyam@gmail.com>
2025-01-15 15:58:57 +0000

    refactored code, removed unnecessary checks

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,34 +1,16 @@
 function findMax(elements) {
-// Given an empty array, return -Infinity
     if (elements.length === 0) {
         return -Infinity;
-
- // Given an array with one number, return that number
-    } else if (elements.length === 1 && typeof elements[0] === "number") {
-        return elements[0];
-
-// Check if the array includes both numerical and non-numerical values
-    } else {
-// Collect only numerical values
-        let numericalElement = [];
-        elements.forEach((item) => {
-            if (typeof item === "number") {
-                numericalElement.push(item);
-            }
-        });
-
-// If no numerical elements are found, return "no numbers exist in the array"
-            if (numericalElement.length === 0) {
-                return  NaN;
-            }
-// Debugging output to check filtered values
-        console.log(numericalElement); 
-
-// Return the maximum numerical value
-        return Math.max(...numericalElement);
     }
-}
 
+    const numericalElement = elements.filter((item) => typeof item === "number");
+
+    if (numericalElement.length === 0) {
+        return NaN;
+    }
+
+    return Math.max(...numericalElement);
+}
 
 const elements = [ "dog", "cat"];
 console.log(findMax(elements));
